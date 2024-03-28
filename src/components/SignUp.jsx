757908a5e61669e1b3175dd018a42abe4f29abc9
kushalkumarghosh <cse.kushal1@gmail.com>
2024-03-28 17:38:06 +0600

    create user to the database

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -12,10 +12,26 @@ const SignUp = () => {
     console.log(email, password);
     createUser(email, password)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
+        //new user has been created
+        const createdAt = result.user?.metadata?.creationTime;
+        const user = { email, createdAt: createdAt };
+        fetch("http://localhost:5000/user", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if(data.insertedId){
+                console.log('user added to the database')
+            }
+          });
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   };
   return (
