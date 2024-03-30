7cf21bc9fb06dfe25f64fda428178e51976554fe
kushalkumarghosh <cse.kushal1@gmail.com>
2024-03-30 14:55:00 +0600

    load data wirh Tanstack Query

---
+++ src/components/Users2.jsx
@@ -0,0 +1,87 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+const Users2 = () => {
+//   const [users, setUsers] = useState([]);
+//   useEffect(() => {
+//     fetch("http://localhost:5000/user")
+//       .then(res => res.json())
+//       .then(data => {
+//         setUsers(data);
+//       })
+//   }, []) 
+
+const { isPending, error, isError, data: users } = useQuery({
+    queryKey: ['users'],
+    queryFn: async() => {
+        const res = await fetch('http://localhost:5000/user');
+        return res.json();
+
+    },
+})
+
+  const handleDelete = (id) => {
+    //make sure user is confirmed to delete
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          console.log("deleted successfully");
+          //remove the user from the UI
+          //   const remainingUsers = users.filter((user) => user._id !== id);
+          //   setUsers(remainingUsers);
+        }
+      });
+  };
+
+  if(isPending){
+    return <span className="loading loading-spinner text-primary"></span>
+  }
+
+  if(isError){
+    return <p>{error.message}</p>
+  }
+
+  return (
+    <div>
+      {/* <h2>Users: {loadedUsers.length}</h2> */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Email</th>
+              <th>createdAt</th>
+              <th>Last Logged In</th>
+              <th>Action</th>
+            </tr>
+          </thead> 
+          <tbody>
+            {/* row 1 */}
+            {users.map((user) => (
+              <tr key={user._id}>
+                <th>1</th>
+                <td>{user.email}</td>
+                <td>{user.createdAt}</td>
+                <td></td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn"
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Users2;
